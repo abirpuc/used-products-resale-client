00e46e1f1d013d42f5fbdbbd1e0c83e0fae6820d
abirpuc <mhabirbd2016@gmail.com>
2022-11-28 11:36:19 +0600

    design all product page using demo data

--- src/Components/Pages/Products/Products.js
+++ src/Components/Pages/Products/Products.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import UseTitle from '../../Hooks/Title/UseTile';
+import ProductsItem from './ProductsItem';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
     UseTitle('Products')
+    
+    useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
+
     return (
-        <div className='mt-50'>
-            <h1>sideNav</h1>
-            <h1>products</h1>
+        <div className='mt-50 '>
+            <div className='grid lg:grid-cols-3 md: gid:-cols-2 sm:grid-rows-1 gap-3'>
+                {
+                    products.map(product => <ProductsItem
+                        key={product._id}
+                        product={product}
+                    ></ProductsItem>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/Pages/Products/ProductsItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ProductsItem = ({ product }) => {
+    const { _id, img, product_name, seller_name, price, description, address, mobile, date } = product
+    return (
+        <div>
+            <div className="card text-neutral card-compact bg-base-100 shadow-xl">
+                <figure><img src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <div className='flex items-center justify-between'>
+                        <h2 className="card-title">{product_name}</h2>
+                        <p className='ml-8 text-xl font-bold'>Price:{price}</p>
+                    </div>
+                    <div>
+                        <p className='text-2xl'>Description:</p>
+                    <p className='text-xl text-neutral indent-2'>{description}</p>
+                    </div>
+                    <p className='text-xl'>Address: {address}</p>
+                    <div className='text-bold flex items-center justify-around'>
+                    <h1>Seller Name: {seller_name}</h1>
+                    <p>Contract No: {mobile}</p>
+                    </div>
+                    <div className="card-actions flex items-center justify-between">
+                        <p className='text-bold'>Release Date: {date}</p>
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductsItem;
